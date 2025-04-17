2d63a1a968f88aea5e1cee600573ce310f779102
nomindnick <nickclair@gmail.com>
2025-04-16 20:06:41 -0700

    feat: Add catchup plan

--- app/planner/routes.py
+++ app/planner/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect, url_for, flash, request
+from flask import render_template, redirect, url_for, flash, request, jsonify, session
 from flask_login import login_required, current_user
 from . import planner_bp
 from ..models import BillableHourGoal, Holiday, VacationDay, DailyLog
